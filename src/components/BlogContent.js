dd083bba7929fbc240e3758d1bea055f5a0ef4a0
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-15 10:03:00 +0300

    Done

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return <div id="blog-content">{props.myName} and {props.title}</div>;
 }
 
 export default BlogContent;
